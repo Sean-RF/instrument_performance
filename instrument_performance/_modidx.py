c995a91872791eda6649ce5fa8433adfec7eea9e
Sean <sean.mckee@colorado.edu>
2024-12-20 14:11:03 -0700

    testing nbdev features, gonna be a lot of worthless commits coming up

--- instrument_performance/_modidx.py
+++ instrument_performance/_modidx.py
@@ -5,4 +5,11 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Sean-RF.github.io',
                 'git_url': 'https://github.com/Sean-RF/instrument_performance',
                 'lib_path': 'instrument_performance'},
-  'syms': {'instrument_performance.core': {'instrument_performance.core.foo': ('core.html#foo', 'instrument_performance/core.py')}}}
+  'syms': { 'instrument_performance.performance_study': { 'instrument_performance.performance_study.Instrument': ( 'core.html#instrument',
+                                                                                                                   'instrument_performance/performance_study.py'),
+                                                          'instrument_performance.performance_study.Instrument.__init__': ( 'core.html#instrument.__init__',
+                                                                                                                            'instrument_performance/performance_study.py'),
+                                                          'instrument_performance.performance_study.Instrument.amethod': ( 'core.html#instrument.amethod',
+                                                                                                                           'instrument_performance/performance_study.py'),
+                                                          'instrument_performance.performance_study.Instrument.anothermethod': ( 'core.html#instrument.anothermethod',
+                                                                                                                                 'instrument_performance/performance_study.py')}}}
